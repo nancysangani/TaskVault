b6be5f75bdb838bde6560df48bf7ca7be6363fe2
nancysangani <9d.24.nancy.sangani@gmail.com>
2026-02-03 00:32:57 +0530

    fix: set token as cookie

--- backend/index.js
+++ backend/index.js
@@ -146,8 +146,25 @@ async function startServer() {
         resp.status(500).json({ success: false, message: "Server error" });
       }
     });
+    function authenticateToken(req, res, next) {
+      const token = req.cookies.token;
+      if (!token)
+        return res
+          .status(401)
+          .json({ success: false, message: "No token provided!" });
+
+      jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+        if (err)
+          return res
+            .status(403)
+            .json({ success: false, message: "Invalid or expired token!" });
+        req.user = user;
+        next();
+      });
+    }
 
     // ---------- TASK ROUTES ----------
+
     app.get("/", authenticateToken, async (req, resp) => {
       const fetchTasks = await tasksCollection
         .find({ userId: req.user.userId })
